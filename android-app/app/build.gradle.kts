c62a93ef0dd5dd97787ef88317a9ae39ed8ef5f1
rick <rjuanjp98@gmail.com>
2026-05-23 23:33:14 +0200

    ci: Align app version with release tags (v1.2.2)

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.gemini.zoomwidget"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.1.3"
+        versionCode = 5
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
