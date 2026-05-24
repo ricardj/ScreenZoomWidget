de02bdb6a98779f87d3c1f4fcfcb6a6d6dbbfe7b
ricardj <43671359+ricardj@users.noreply.github.com>
2026-05-24 14:21:16 +0200

    chore: bump version to 1.4.2

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.gemini.zoomwidget"
         minSdk = 26
         targetSdk = 26
-        versionCode = 10
-        versionName = "1.4.1"
+        versionCode = 11
+        versionName = "1.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
